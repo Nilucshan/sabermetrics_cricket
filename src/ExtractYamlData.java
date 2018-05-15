4b383e0e4189fd9a292cc9f6f1e63e134a8d8bd0
snilucshan <tonilucshan@gmail.com>
2018-05-15 20:10:13 +0530

    A sample program to extract data from .yaml files.

---
+++ src/ExtractYamlData.java
@@ -0,0 +1,37 @@
+/**
+ * Created by Nilucshan on 5/15/2018.
+ */
+
+import java.io.File;
+
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
+public class ExtractYamlData {
+
+    public static void main(String[] args) {
+
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+
+        try {
+
+            User user = mapper.readValue(new File("user.yaml"), User.class);
+
+            System.out.println(ReflectionToStringBuilder.toString(user,ToStringStyle.MULTI_LINE_STYLE));
+
+        } catch (Exception e) {
+
+            // TODO Auto-generated catch block
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+}
